97987c78cc385763ed590c9807f4cd90c65c7c98
Yaroslav Klimas <yarklim@gmail.com>
2023-11-21 17:26:43 +0200

    update style.css

---
+++ main.js
@@ -0,0 +1,8 @@
+const display = document.querySelector('.display');
+const buttons = Array.from(document.querySelectorAll('.button'));
+
+buttons.map((button) => {
+  button.addEventListener('click', (e) => {
+    console.log(e.target.innerText);
+  });
+});
